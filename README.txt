a539d66a341a8719f2b0da99f804d95113d0ea57
DaniaCerpa <dania.cerpa@gmail.com>
2024-09-15 13:42:25 -0300

    Primer commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Alumno: Dania Cerpa
+Curso: BOTIC-SOFOF-23-30-13-0068 -DESARROLLO DE APLICACIONES FULL STACK PYTHON TRAINEE V2.0
+Modulo: 4
+Sesión: 8
+Tarea tipo: Evaluación
+Repositorio Github: https://github.com/DaniaCerpa/Evaluacion-M4S8.git
+Clonar repositorio: git clone https://github.com/DaniaCerpa/Evaluacion-M4S8.git
---
+++ leer_informacion.py
@@ -0,0 +1,34 @@
+def leer_informacion(archivo:str, formato:str):
+    """Lee el contenido de un archivo previamente creado y muestra su contenido.
+    Si el archivo no existe o no se encuentra, se captura una excepción FileNotFoundError y se informa al usuario.
+   
+    Args:
+        archivo: nombre del archivo al cual se intenta acceder\n
+        formato: formato del archivo al cual se intenta acceder
+    """
+    try:
+        ruta = "./archivos/"
+        nom_archivo = f"{archivo}.{formato}"
+        path = ruta + nom_archivo
+        
+        with open(path, "r") as nuevo_archivo:
+            print(f"El archivo {archivo}.{formato} ya existe, ha sido creado previamente")
+            for lineas in nuevo_archivo:
+                print(lineas.strip()) #funcion strip() elimina los espacios en blanco entre lineas.
+                
+         
+    except FileNotFoundError:
+        print(f"El archivo al cual intentas acceder no existe o no se encuentra dentro de la ruta especificada")
+
+    
+    
+def main():
+    """Función principal del programa que entrega los parametros necesarios para la posterior ejecución de la función leer_informacion().
+    """
+    nombre_archivo = "informacion"
+    formato = "dat"    
+    
+    leer_informacion(nombre_archivo, formato)
+
+#Ejecución de la funcion para correr el programa
+main()
